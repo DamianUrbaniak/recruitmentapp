65d67877a52df88f1db76ed09d7a16b3f5090f82
damian.urbaniak <damian.urbaniak@antologic.com>
2024-02-13 21:02:41 +0100

    initial commit

---
+++ src/main/java/com/damianurbaniak/recruitmentapp/RecruitmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.damianurbaniak.recruitmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentApiApplication.class, args);
+	}
+
+}
